d368f6e178dcf8de126353c8c7f12a6ab886eae8
Patrick Carlisle <patrick@puppetlabs.com>
2016-01-21 17:49:26 -0800

    Add eastwood to lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,6 +6,7 @@
                        [org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/clojure "1.7.0"]
                        ]}
+ :user {:plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]}
 
  :test {:plugins [[pjstadig/humane-test-output "0.7.0"]]}
  }
