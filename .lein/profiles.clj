b2bb4a1273ba0e605a46a3e65c519e41fefdfcea
Patrick Carlisle <patrick@puppetlabs.com>
2016-02-18 11:32:59 -0800

    Update lein dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,12 +1,15 @@
-{:repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
-                  ; [lein-cljfmt "0.1.10"]
+{:repl {:plugins [
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [refactor-nrepl "2.2.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
                   ]
-        :dependencies [[alembic "0.3.2"]
+        :dependencies [
+                       [alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]
-                       [org.clojure/clojure "1.7.0"]
+                       ; [org.clojure/clojure "1.8.0"]
                        ]}
  :user {:plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]}
 
- :test {:plugins [[pjstadig/humane-test-output "0.7.0"]]}
+ :test {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}
  }
