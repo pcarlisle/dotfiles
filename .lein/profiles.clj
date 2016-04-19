6f9d737837badbeb9330dbc81a2f1757f6052099
Patrick Carlisle <patrick@puppetlabs.com>
2016-04-19 10:25:36 -0700

    Update lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,15 +1,21 @@
 {:repl {:plugins [
-                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.2.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+                  [cider/cider-nrepl "0.12.0"]
+                  [refactor-nrepl "2.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
                   ]
         :dependencies [
                        [alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]
                        ; [org.clojure/clojure "1.8.0"]
                        ]}
- :user {:plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]}
+ :user {:plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+                  [venantius/yagni "0.1.4" :exclusions [org.clojure/clojure]]
+                  ;; [io.aviso/pretty "0.1.26" :exclusions [org.clojure/clojure]]
+                  ]
+        ;; :dependencies [[io.aviso/pretty "0.1.26"]]
+        }
 
- :test {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}
+ ;; :test {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
+ ;;        :injections [(require 'pjstadig.humane-test-output)
+ ;;                     (pjstadig.humane-test-output/activate!)]
+
  }
