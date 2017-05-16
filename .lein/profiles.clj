ad5655b95fb11d7a82f82af1a1080f30f8009856
Patrick Carlisle <patrick@puppetlabs.com>
2017-05-16 11:33:20 -0700

    Add lein pretty profile, update nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,23 +1,32 @@
 {:repl {:plugins [
-                  [cider/cider-nrepl "0.13.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.14.0-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
                   ]
         :dependencies [
                        [alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]
-                       ; [org.clojure/clojure "1.8.0"]
+                       ;; [org.clojure/clojure "1.8.0"]
                        ]
         :jvm-opts ["-Xmx512m"]
         }
  :user {:plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
                   [venantius/yagni "0.1.4" :exclusions [org.clojure/clojure]]
-                  ;; [io.aviso/pretty "0.1.26" :exclusions [org.clojure/clojure]]
+                  ; [lein-ancient "0.6.10"]
                   ]
-        ;; :dependencies [[io.aviso/pretty "0.1.26"]]
+        :dependencies [
+                       [spyscope "0.1.5"]
+                       ]
+        :injections [(require 'spyscope.core)]
         }
 
  ;; :test {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
  ;;        :injections [(require 'pjstadig.humane-test-output)
  ;;                     (pjstadig.humane-test-output/activate!)]
 
+ :ci {:plugins [[lein-pprint "1.1.1"]]}
+
+ :pretty {
+  :plugins [[io.aviso/pretty "0.1.33"]]
+  :dependencies [[io.aviso/pretty "0.1.33"]]
+}
  }
