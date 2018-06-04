499b2ac9212b2e4bd79515b313533e8d3f127b9a
Patrick Carlisle <patrick@puppetlabs.com>
2018-06-04 16:52:46 -0700

    Update lein profiles
    Add some puppetserver settings, eftest, bump cider versions

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,22 +1,28 @@
 {:repl {:plugins [
-                  [cider/cider-nrepl "0.14.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
-                  [refactor-nrepl "2.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+                  [cider/cider-nrepl "0.17.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+                  [refactor-nrepl "2.4.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+                  ;; [com.billpiel/sayid "0.0.15"]
                   ]
         :dependencies [
                        [alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]
+                       [org.clojure/tools.nrepl "0.2.13"]
                        ;; [org.clojure/clojure "1.8.0"]
                        ]
-        :jvm-opts ["-Xmx512m"]
+        :jvm-opts ["-Xms512m" "-Xmx512m"]
         }
  :user {:plugins [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-                  [venantius/yagni "0.1.4" :exclusions [org.clojure/clojure]]
+                  ; [venantius/yagni "0.1.4" :exclusions [org.clojure/clojure]]
                   ; [lein-ancient "0.6.10"]
+                  [lein-eftest "0.5.0"]
                   ]
         :dependencies [
                        [spyscope "0.1.5"]
+                       [eftest "0.5.0"]
                        ]
         :injections [(require 'spyscope.core)]
+        :puppetserver-heap-size "5G"
+        :eftest {
+                 :test-warn-time 500}
         }
 
  ;; :test {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
