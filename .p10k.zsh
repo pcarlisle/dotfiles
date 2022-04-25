4a94524c1dac899515cfbb1c297a92421ec7a038
Patrick Carlisle <pcarlisle@github.com>
2022-04-25 15:28:53 -0700

    Turn on p10k shell integration

--- .p10k.zsh
+++ .p10k.zsh
@@ -1577,3 +1577,4 @@ typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
 (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
 'builtin' 'unset' 'p10k_config_opts'
+typeset -g POWERLEVEL9K_TERM_SHELL_INTEGRATION=true
